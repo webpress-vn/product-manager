93ab70fe84c6543b891e91f39eec0d6ecd68ccaf
vietanhxan <vietanhxan@gmail.com>
2020-11-11 09:27:55 +0700

    remove code in function down()

--- src/database/migrations/2020_11_11_022107_delete_brand_column_in_products_table.php
+++ src/database/migrations/2020_11_11_022107_delete_brand_column_in_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class DeleteBrandColumnFromProductsTable extends Migration
+class DeleteBrandColumnInProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -25,8 +25,6 @@ class DeleteBrandColumnFromProductsTable extends Migration
      */
     public function down()
     {
-        Schema::table('products', function (Blueprint $table) {
-            $table->string('brand')->nullable();
-        });
+        //
     }
 }
